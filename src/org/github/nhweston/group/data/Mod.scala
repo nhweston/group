69241dd6a977cf91e2ec6448feb6cbb08cad6582
nhweston <nhweston1997@gmail.com>
2022-04-30 20:47:18 +1000

    override equality for 'Mod'

--- src/org/github/nhweston/group/data/Mod.scala
+++ src/org/github/nhweston/group/data/Mod.scala
@@ -8,6 +8,14 @@ class Mod[M <: Int] private (val value: Int, val modulus: M):
 
   override def toString = value.toString
 
+  override def equals(other: Any): Boolean =
+    other match
+      case other: Mod[?] => this.value == other.value
+      case _ => false
+  
+  override def hashCode: Int =
+    (value, modulus).hashCode
+
 object Mod:
 
   def apply[M <: Int](value: Int)(using m: ValueOf[M]): Mod[M] =
